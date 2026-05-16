3ec9f1dd053631bc4ecebacae0490cd567425056
Luis R Azuara <ricky_6b@hotmail.com>
2026-05-16 17:55:39 -0400

    Project set up

---
+++ BackendAssigment/Program.cs
@@ -0,0 +1,10 @@
+﻿//variables
+enum symbols { sym1, sym2, sym3, sym4, sym5, sym6, sym7, sym8 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("pene");
+    }
+}
