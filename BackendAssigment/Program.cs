0343661b65e792de84d1df9c5a8eba0a95cf54b9
Luis R Azuara <ricky_6b@hotmail.com>
2026-05-18 20:44:09 -0400

    Second assigment finished, missing cleaning and documentation

--- BackendAssigment/Program.cs
+++ BackendAssigment/Program.cs
@@ -4,67 +4,67 @@ enum Symbols { sym1, sym2, sym3, sym4, sym5, sym6, sym7, sym8 }
 class Program
 {
     //bands
-    static string[] band1 = new string[] { Symbols.sym2.ToString(),Symbols.sym7.ToString(),
-                                    Symbols.sym7.ToString(), Symbols.sym1.ToString(),
-                                    Symbols.sym1.ToString(), Symbols.sym5.ToString(),
-                                    Symbols.sym1.ToString(), Symbols.sym4.ToString(),
-                                    Symbols.sym5.ToString(), Symbols.sym3.ToString(),
-                                    Symbols.sym2.ToString(), Symbols.sym3.ToString(),
-                                    Symbols.sym8.ToString(), Symbols.sym4.ToString(),
-                                    Symbols.sym5.ToString(), Symbols.sym2.ToString(),
-                                    Symbols.sym8.ToString(), Symbols.sym5.ToString(),
-                                    Symbols.sym7.ToString(), Symbols.sym2.ToString() };
-    static string[] band2 = new string[] { Symbols.sym1.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym7.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym5.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym8.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym5.ToString(), Symbols.sym4.ToString(),
-                                           Symbols.sym7.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym5.ToString(), Symbols.sym7.ToString(),
-                                           Symbols.sym1.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym8.ToString(),
-                                           Symbols.sym7.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym3.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym7.ToString(),
-                                           Symbols.sym3.ToString() };
-    static string[] band3 = new string[] { Symbols.sym5.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym7.ToString(), Symbols.sym8.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym2.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym1.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym4.ToString(), Symbols.sym1.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym8.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym3.ToString(),
-                                           Symbols.sym4.ToString(), Symbols.sym4.ToString(),
-                                           Symbols.sym8.ToString(), Symbols.sym1.ToString(),
-                                           Symbols.sym7.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym1.ToString(), Symbols.sym6.ToString() };
-    static string[] band4 = new string[] { Symbols.sym2.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym8.ToString(), Symbols.sym8.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym8.ToString(), Symbols.sym1.ToString(),
-                                           Symbols.sym5.ToString(), Symbols.sym1.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym3.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym7.ToString(),
-                                           Symbols.sym2.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym8.ToString(), Symbols.sym4.ToString(),
-                                           Symbols.sym1.ToString(), Symbols.sym5.ToString(),
-                                           Symbols.sym7.ToString() };
-    static string[] band5 = new string[] { Symbols.sym7.ToString(), Symbols.sym8.ToString(),
-                                           Symbols.sym2.ToString(), Symbols.sym3.ToString(),
-                                           Symbols.sym4.ToString(), Symbols.sym1.ToString(),
-                                           Symbols.sym3.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym2.ToString(), Symbols.sym4.ToString(),
-                                           Symbols.sym4.ToString(), Symbols.sym2.ToString(),
-                                           Symbols.sym6.ToString(), Symbols.sym4.ToString(),
-                                           Symbols.sym1.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym1.ToString(), Symbols.sym6.ToString(),
-                                           Symbols.sym4.ToString(), Symbols.sym8.ToString() };
+    static int[] band1 = new int[] { (int)Symbols.sym2, (int)Symbols.sym7,
+                                     (int)Symbols.sym7, (int)Symbols.sym1,
+                                     (int)Symbols.sym1, (int)Symbols.sym5,
+                                     (int)Symbols.sym1, (int)Symbols.sym4,
+                                     (int)Symbols.sym5, (int)Symbols.sym3,
+                                     (int)Symbols.sym2, (int)Symbols.sym3,
+                                     (int)Symbols.sym8, (int)Symbols.sym4,
+                                     (int)Symbols.sym5, (int)Symbols.sym2,
+                                     (int)Symbols.sym8, (int)Symbols.sym5,
+                                     (int)Symbols.sym7, (int)Symbols.sym2 };
+    static int[] band2 = new int[] { (int)Symbols.sym1, (int)Symbols.sym6,
+                                     (int)Symbols.sym7, (int)Symbols.sym6,
+                                     (int)Symbols.sym5, (int)Symbols.sym5,
+                                     (int)Symbols.sym8, (int)Symbols.sym5,
+                                     (int)Symbols.sym5, (int)Symbols.sym4,
+                                     (int)Symbols.sym7, (int)Symbols.sym2,
+                                     (int)Symbols.sym5, (int)Symbols.sym7,
+                                     (int)Symbols.sym1, (int)Symbols.sym5,
+                                     (int)Symbols.sym6, (int)Symbols.sym8,
+                                     (int)Symbols.sym7, (int)Symbols.sym6,
+                                     (int)Symbols.sym3, (int)Symbols.sym3,
+                                     (int)Symbols.sym6, (int)Symbols.sym7,
+                                     (int)Symbols.sym3 };
+    static int[] band3 = new int[] { (int)Symbols.sym5, (int)Symbols.sym2,
+                                     (int)Symbols.sym7, (int)Symbols.sym8,
+                                     (int)Symbols.sym3, (int)Symbols.sym2,
+                                     (int)Symbols.sym6, (int)Symbols.sym2,
+                                     (int)Symbols.sym2, (int)Symbols.sym5,
+                                     (int)Symbols.sym3, (int)Symbols.sym5,
+                                     (int)Symbols.sym1, (int)Symbols.sym6,
+                                     (int)Symbols.sym3, (int)Symbols.sym2,
+                                     (int)Symbols.sym4, (int)Symbols.sym1,
+                                     (int)Symbols.sym6, (int)Symbols.sym8,
+                                     (int)Symbols.sym6, (int)Symbols.sym3,
+                                     (int)Symbols.sym4, (int)Symbols.sym4,
+                                     (int)Symbols.sym8, (int)Symbols.sym1,
+                                     (int)Symbols.sym7, (int)Symbols.sym6,
+                                     (int)Symbols.sym1, (int)Symbols.sym6 };
+    static int[] band4 = new int[] { (int)Symbols.sym2, (int)Symbols.sym6,
+                                     (int)Symbols.sym3, (int)Symbols.sym6,
+                                     (int)Symbols.sym8, (int)Symbols.sym8,
+                                     (int)Symbols.sym3, (int)Symbols.sym6,
+                                     (int)Symbols.sym8, (int)Symbols.sym1,
+                                     (int)Symbols.sym5, (int)Symbols.sym1,
+                                     (int)Symbols.sym6, (int)Symbols.sym3,
+                                     (int)Symbols.sym6, (int)Symbols.sym7,
+                                     (int)Symbols.sym2, (int)Symbols.sym5,
+                                     (int)Symbols.sym3, (int)Symbols.sym6,
+                                     (int)Symbols.sym8, (int)Symbols.sym4,
+                                     (int)Symbols.sym1, (int)Symbols.sym5,
+                                     (int)Symbols.sym7 };
+    static int[] band5 = new int[] { (int)Symbols.sym7, (int)Symbols.sym8,
+                                     (int)Symbols.sym2, (int)Symbols.sym3,
+                                     (int)Symbols.sym4, (int)Symbols.sym1,
+                                     (int)Symbols.sym3, (int)Symbols.sym2,
+                                     (int)Symbols.sym2, (int)Symbols.sym4,
+                                     (int)Symbols.sym4, (int)Symbols.sym2,
+                                     (int)Symbols.sym6, (int)Symbols.sym4,
+                                     (int)Symbols.sym1, (int)Symbols.sym6,
+                                     (int)Symbols.sym1, (int)Symbols.sym6,
+                                     (int)Symbols.sym4, (int)Symbols.sym8 };
     
     //symbols
     static string symbSelected1 = "", symbSelected1Bottom1 = "", symbSelected1Bottom2 = "";
@@ -73,10 +73,50 @@ class Program
     static string symbSelected4 = "", symbSelected4Bottom1 = "", symbSelected4Bottom2 = "";
     static string symbSelected5 = "", symbSelected5Bottom1 = "", symbSelected5Bottom2 = "";
 
-    static int getRandomIndex(string[] band)
+    static int colNumber = 5;
+    static int rowNumber = 3;
+    static int[,] symbGrid = new int[rowNumber, colNumber];
+    static Dictionary<Symbols, int[]> payTable = new Dictionary<Symbols, int[]>()
+    {
+        { Symbols.sym1, new int[]{0,0,1,2,3} },
+        { Symbols.sym2, new int[]{0,0,1,2,3} },
+        { Symbols.sym3, new int[]{0,0,1,2,5} },
+        { Symbols.sym4, new int[]{0,0,2,5,10} },
+        { Symbols.sym5, new int[]{0,0,5,10,15} },
+        { Symbols.sym6, new int[]{0,0,5,10,15} },
+        { Symbols.sym7, new int[]{0,0,5,10,20} },
+        { Symbols.sym8, new int[]{0,0,10,20,50} }
+    };
+
+    static int getRandomIndex(int[] band)
     {
         Random random = new Random();
         return random.Next(0, band.Length);
+    }
+
+    static List<List<int>> FindCombinations(List<List<int>> sequences)
+    {
+        List<List<int>> results = new List<List<int>>();
+
+            void Recurse(int index, List<int> currentComb)
+            {
+                //Check full combination
+                if (index == sequences.Count)
+                {
+                    results.Add(new List<int>(currentComb));
+                    return;
+                }
+
+                foreach (int item in sequences[index])
+                {
+                    currentComb.Add(item);
+                    Recurse(index + 1, currentComb);
+                    currentComb.RemoveAt(currentComb.Count - 1);
+                }
+            }
+
+            Recurse(0, new List<int>());
+        return results;
     }
 
     static void Main(string[] args)
@@ -87,29 +127,102 @@ class Program
         int index4 = getRandomIndex(band4);
         int index5 = getRandomIndex(band5);
 
-        symbSelected1 = band1[index1];
-        symbSelected1Bottom1 = band1[(index1 + 1) % band1.Length];
-        symbSelected1Bottom2 = band1[(index1 + 2) % band1.Length];
+        symbGrid[0, 0] = band1[index1];
+        symbGrid[0, 1] = band2[index2];
+        symbGrid[0, 2] = band3[index3];
+        symbGrid[0, 3] = band4[index4];
+        symbGrid[0, 4] = band5[index5];
 
-        symbSelected2 = band2[index2];
-        symbSelected2Bottom1 = band2[(index2 + 1) % band2.Length];
-        symbSelected2Bottom2 = band2[(index2 + 2) % band2.Length];
+        symbGrid[1, 0] = band1[(index1 + 1) % band1.Length];
+        symbGrid[1, 1] = band2[(index2 + 1) % band2.Length];
+        symbGrid[1, 2] = band3[(index3 + 1) % band3.Length];
+        symbGrid[1, 3] = band4[(index4 + 1) % band4.Length];
+        symbGrid[1, 4] = band5[(index5 + 1) % band5.Length];
 
-        symbSelected3 = band3[index3];
-        symbSelected3Bottom1 = band3[(index3 + 1) % band3.Length];
-        symbSelected3Bottom2 = band3[(index3 + 2) % band3.Length];
+        symbGrid[2, 0] = band1[(index1 + 2) % band1.Length];
+        symbGrid[2, 1] = band2[(index2 + 2) % band2.Length];
+        symbGrid[2, 2] = band3[(index3 + 2) % band3.Length];
+        symbGrid[2, 3] = band4[(index4 + 2) % band4.Length];
+        symbGrid[2, 4] = band5[(index5 + 2) % band5.Length];
 
-        symbSelected4 = band4[index4];
-        symbSelected4Bottom1 = band4[(index4 + 1) % band4.Length];
-        symbSelected4Bottom2 = band4[(index4 + 2) % band4.Length];
+        symbSelected1 = ((Symbols)symbGrid[0, 0]).ToString();
+        symbSelected1Bottom1 = ((Symbols)symbGrid[1, 0]).ToString();
+        symbSelected1Bottom2 = ((Symbols)symbGrid[2, 0]).ToString();
 
-        symbSelected5 = band5[index5];
-        symbSelected5Bottom1 = band5[(index5 + 1) % band5.Length];
-        symbSelected5Bottom2 = band5[(index5 + 2) % band5.Length];
+        symbSelected2 = ((Symbols)symbGrid[0, 1]).ToString();
+        symbSelected2Bottom1 = ((Symbols)symbGrid[1, 1]).ToString();
+        symbSelected2Bottom2 = ((Symbols)symbGrid[2, 1]).ToString();
 
-        Console.WriteLine("Stop Positions: " + index1 + ", " + index2 + ", " + index3 + ", " + index4 + ", " + index5);
-        Console.WriteLine(symbSelected1 + " " + symbSelected2 + " " + symbSelected3 + " " + symbSelected4 + " " + symbSelected5);
-        Console.WriteLine(symbSelected1Bottom1 + " " + symbSelected2Bottom1 + " " + symbSelected3Bottom1 + " " + symbSelected4Bottom1 + " " + symbSelected5Bottom1);
-        Console.WriteLine(symbSelected1Bottom2 + " " + symbSelected2Bottom2 + " " + symbSelected3Bottom2 + " " + symbSelected4Bottom2 + " " + symbSelected5Bottom2);
+        symbSelected3 = ((Symbols)symbGrid[0, 2]).ToString();
+        symbSelected3Bottom1 = ((Symbols)symbGrid[1, 2]).ToString();
+        symbSelected3Bottom2 = ((Symbols)symbGrid[2, 2]).ToString();
+
+
+        symbSelected4 = ((Symbols)symbGrid[0, 3]).ToString();
+        symbSelected4Bottom1 = ((Symbols)symbGrid[1, 3]).ToString();
+        symbSelected4Bottom2 = ((Symbols)symbGrid[2, 3]).ToString();
+
+        symbSelected5 = ((Symbols)symbGrid[0, 4]).ToString();
+        symbSelected5Bottom1 = ((Symbols)symbGrid[1, 4]).ToString();
+        symbSelected5Bottom2 = ((Symbols)symbGrid[2, 4]).ToString();
+
+        Console.WriteLine($"Stop Positions: {index1}, {index2}, {index3}, {index4}, {index5}");
+        Console.WriteLine($"{symbSelected1} {symbSelected2} {symbSelected3} {symbSelected4} {symbSelected5}");
+        Console.WriteLine($"{symbSelected1Bottom1} {symbSelected2Bottom1} {symbSelected3Bottom1} {symbSelected4Bottom1} {symbSelected5Bottom1}");
+        Console.WriteLine($"{symbSelected1Bottom2} {symbSelected2Bottom2} {symbSelected3Bottom2} {symbSelected4Bottom2} {symbSelected5Bottom2}");
+
+        int totalWin = 0;
+        List<string> winDetails = new List<string>();
+
+        for (int row = 0; row < rowNumber; row++)
+        {
+            int symbolId = symbGrid[row, 0];
+            Symbols sym = (Symbols)symbolId;
+
+            //Store matching rows per column
+            List<List<int>> waysPositions = new List<List<int>>();
+            waysPositions.Add(new List<int> { row });
+
+            int consecutiveCols = 1;
+
+            for (int col = 1; col < colNumber; col++)
+            {
+                List<int> matchingRows = new List<int>();
+                for (int r = 0; r < rowNumber; r++)
+                {
+                    if (symbGrid[r, col] == symbolId)
+                        matchingRows.Add(r);
+                }
+
+                if (matchingRows.Count == 0)
+                    break;
+
+                waysPositions.Add(matchingRows);
+                consecutiveCols++;
+            }
+
+            if (consecutiveCols >= 3)
+            {
+                int payout = payTable[sym][consecutiveCols];
+
+                List<List<int>> allCombinations = FindCombinations(waysPositions);
+
+                foreach (List<int> combo in allCombinations)
+                {
+                    List<int> positions = new List<int>();
+                    for (int c = 0; c < combo.Count; c++)
+                    {
+                        positions.Add(combo[c] * colNumber + c);
+                    }
+
+                    winDetails.Add($"- Ways win {string.Join("-", positions)}, {sym} x {consecutiveCols}, {payout}");
+                    totalWin += payout;
+                }
+            }
+        }
+
+        Console.WriteLine("Total wins: " + totalWin);
+        foreach (string w in winDetails)
+            Console.WriteLine(w);
     }
 }
